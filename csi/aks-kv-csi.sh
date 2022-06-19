a4f4819e7b1d38b55068269f7aae4efa749a5116
pradeepkotha84 <105610941+pradeepkotha84@users.noreply.github.com>
2022-06-19 22:34:23 +0100

    Update aks-kv-csi.sh

--- csi/aks-kv-csi.sh
+++ csi/aks-kv-csi.sh
@@ -8,4 +8,6 @@ az aks show -g <resource-group> -n <cluster-name> --query addonProfiles.azureKey
 
 # take the client id and assign aks permissions
 
-
+# old way
+helm repo add csi-secrets-store-provider-azure https://azure.github.io/secrets-store-csi-driver-provider-azure/charts
+helm install csi csi-secrets-store-provider-azure/csi-secrets-store-provider-azure
